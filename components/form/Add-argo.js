ecb332069f0b35a5c04925820bc21309b37499d8
Marjorie Meziere <mmwebforce20@gmail.com>
2021-07-17 02:49:56 +0200

    refactoring, react hook form added

--- components/form/Add-argo.js
+++ components/form/Add-argo.js
@@ -1,61 +1,79 @@
 import classes from "./Add-argo.module.css";
 import { useState, useEffect } from "react";
 import ArgoList from "../list/Argo-list";
+import { useForm } from "react-hook-form";
 
-function AddArgo(props) {
-	const [name, setName] = useState("");
+function AddArgo() {
 	const [argos, setArgos] = useState([]);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm();
 
-	function submitHandler(e) {
-		e.preventDefault();
+	const onSubmit = (data) => {
 		setArgos([
 			...argos,
 			{
 				id: argos.length + 1,
-				name: name,
+				name: data.name,
 			},
 		]);
-		props.handleSubmit();
-		setName("");
-	}
+
+		// props.handleSubmit();
+		reset();
+	};
+
 	useEffect(() => {
-		// console.log(argos.length);
 		if (argos.length < 20) {
 			localStorage.setItem("formData", JSON.stringify(argos));
 		} else {
 			console.log(stop);
 		}
-		// console.log(JSON.parse(localStorage.getItem("formData")));
+		// console.log("local", JSON.parse(localStorage.getItem("formData")));
 	}, [argos]);
 
 	return (
 		<div className="container px-5">
 			<h2 className="text-center py-5">Ajouter un(e) Argonaute</h2>
-			<form onSubmit={submitHandler}>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<div className="row g-3">
 					<div className="col-12">
 						<div className="text-center">
 							<label htmlFor="name">Nom de l&apos;Argonaute</label>
 						</div>
 					</div>
-					<div className="col-12 col-md-8">
+					<div className="col-12 col-md-8 ">
 						<input
+							{...register("name", {
+								required: "Veuillez entrer un nom pour votre Argonaute.",
+								maxLength: {
+									value: 20,
+									message: "Veuillez entrer un nom entre 3 et 20 charactères.",
+								},
+								minLength: {
+									value: 3,
+									message: "Veuillez entrer un nom entre 2 et 20 charactères.",
+								},
+							})}
 							type="text"
 							className="form-control"
-							required
 							id="name"
 							placeholder="Charalampos"
 							aria-label="name"
-							value={name}
-							onChange={(e) => setName(e.target.value)}
 						/>
+
+						{errors.name && (
+							<div className={classes.errors}>{errors.name.message}</div>
+						)}
 					</div>
 					<div className="col-12 col-md-4 d-grid gap-2">
 						{argos.length < 20 ? (
 							<button className={`btn ${classes.button}`}>Envoyer</button>
 						) : (
 							<button className={`text-nowrap btn ${classes.button}`} disabled>
-								Votre équipe est complète !
+								Votre équipage est complet !
 							</button>
 						)}
 					</div>
