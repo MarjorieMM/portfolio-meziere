af033a12355dd4d913af51e4844918b6b8b79281
Marjorie Meziere <mmwebforce20@gmail.com>
2021-07-18 23:29:31 +0200

    fix padding

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -5,7 +5,7 @@ function Header() {
 	const imageSource =
 		"https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png";
 	return (
-		<header className={`text-center py-5 ${classes.header}`}>
+		<header className={`text-center py-4 ${classes.header}`}>
 			<h1>
 				<Image
 					src={imageSource}
