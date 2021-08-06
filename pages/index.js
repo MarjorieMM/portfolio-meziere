bf9a6781a0d4a7395fb43dee8b2b54cdc6cdd9d9
Marjorie Meziere <mmwebforce20@gmail.com>
2021-08-06 23:30:27 +0200

    seo

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,23 @@ function Home(props) {
 					name="description"
 					content="Bienvenue sur le Portfolio de Marjorie MEZIERE"
 				/>
+				<meta
+					property="og:description"
+					content="le Portfolio de Marjorie MEZIERE, avec ses réalisations, son CV et un blog en construction"
+				/>
+				<meta
+					property="og:title"
+					content="Bienvenue sur le Portfolio de Marjorie MEZIERE"
+				/>
+				<meta
+					property="og:url"
+					content="https://protfolio-meziere.herokuapp.com/"
+				/>
+				<meta
+					property="og:image"
+					content="https://burst.shopifycdn.com/photos/kitty-cat-helps-at-work.jpg?width=373&format=pjpg&exif=0&iptc=0"
+				/>
+				<meta property="og:type" content="site web" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
